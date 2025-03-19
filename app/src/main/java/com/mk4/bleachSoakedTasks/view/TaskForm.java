bf4a07c87c9bdc068901bd381eb3a4412569f701
arthur-manske <arthur-manske@proton.me>
2025-03-19 12:34:57 -0300

    REFACTOR(VIEW): Delete main method from TaskForm

--- app/src/main/java/com/mk4/bleachSoakedTasks/view/TaskForm.java
+++ app/src/main/java/com/mk4/bleachSoakedTasks/view/TaskForm.java
@@ -179,14 +179,6 @@ public class TaskForm extends javax.swing.JFrame {
         this.expirationDateSpinner.setEnabled(this.expirationDateCheckbox.isSelected());
     }//GEN-LAST:event_expirationDateCheckboxActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        SwingConfig.setFlatLafTheme(true, null);
-        new TaskForm().setVisible(true);
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JSplitPane Main;
     private javax.swing.JTextArea descriptionArea;
