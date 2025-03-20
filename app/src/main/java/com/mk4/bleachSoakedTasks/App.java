2ce95aa63deb0def3b9b205ae3a1b428df7d7a24
arthur-manske <arthur-manske@proton.me>
2025-03-20 11:20:47 -0300

    FEAT(App): Use WindowPreferencesController over SwingConfig

--- app/src/main/java/com/mk4/bleachSoakedTasks/App.java
+++ app/src/main/java/com/mk4/bleachSoakedTasks/App.java
@@ -1,21 +1,23 @@
 package com.mk4.bleachSoakedTasks;
 
-import com.mk4.bleachSoakedTasks.view.SwingConfig;
+import com.mk4.bleachSoakedTasks.controller.WindowPreferencesController;
+
 import com.mk4.bleachSoakedTasks.view.TaskForm;
 
 /**
- * 
+ * Initializes the program main window
  * @author Arthur de Souza Manske
  */
 public class App {
-    
     /**
      * Initializes the application
      * @param args - Command line arguments
      */
     public static void main(String[] args)
     {
-        SwingConfig.setFlatLafTheme(false, null);        
-        new TaskForm().setVisible(true);
+    	final var app = new App();
+	final var winPrefsController = new WindowPreferencesController(app.getClass().getName());
+	
+	new TaskForm(winPrefsController).setVisible(true);
     }
 }
