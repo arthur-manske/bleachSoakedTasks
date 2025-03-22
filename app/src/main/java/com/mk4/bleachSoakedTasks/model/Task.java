aca49cbb22b16a8b9f7f2573fcdcddf137b85fb1
Arthur Manske <arthur-manske@proton.me>
2025-03-21 21:25:06 -0300

    ADD(MODEL/Task): Creation of task model

---
+++ app/src/main/java/com/mk4/bleachSoakedTasks/model/Task.java
@@ -0,0 +1,87 @@
+package com.mk4.bleachSoakedTasks.model;
+
+import java.util.Date;
+
+/**
+ * Task model
+ * @author Natan Gabriel Wildner
+ */
+public class Task {
+    public enum Status {
+        PENDING, COMPLETED
+    };
+
+    private int    id;
+    private String title;
+    private String description;
+    private Date   expirationDate;
+    private Status status;
+
+    public Task(String title, String description, Date expirationDate, Status status)
+    {
+        this.title          = title;
+        this.description    = description;
+        this.status         = status;
+        this.expirationDate = expirationDate;
+    }
+    
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public Date getExpirationDate()
+    {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate)
+    {
+        this.expirationDate = expirationDate;
+    }
+
+    public Status getStatus()
+    {
+        return status;
+    }
+    
+    public void setStatus(Status status)
+    {
+        this.status = status;
+    }
+
+    public boolean isCompleted()
+    {
+        return this.status == Status.COMPLETED;
+    }
+
+    public boolean hasExpirationDate()
+    {
+        return this.expirationDate != null;
+    }
+}
